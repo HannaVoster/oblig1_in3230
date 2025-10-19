5b923fb481e99a4a812258680f6e849303cc896b
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-10-19 15:03:02 +0200

    h

--- include/iface.h
+++ include/iface.h
@@ -5,7 +5,7 @@
 #define MAX_IFACES 10
 extern int iface_indices[5];
 extern int iface_count;
-extern char iface_names[MAX_IFACES][IFNAMSIZ];  // Navnet på nettverksinterfacet som brukes (f.eks. "A-eth0")
+extern char iface_name[MAX_IFACES][IFNAMSIZ];  // Navnet på nettverksinterfacet som brukes (f.eks. "A-eth0")
 
 int get_iface_mac(const char *ifname, unsigned char *mac);
 void find_all_ifaces(void);
