09b2eab9673d01885bcc80ec7d8c0bf708726f7b
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-10-26 19:22:40 +0100

    sjekke

--- include/raw_handler.h
+++ include/raw_handler.h
@@ -3,7 +3,7 @@
 
 void handle_raw_packet(int raw_sock, int my_mip_address);
 
-void handle_ping_message(int raw_sock, int my_mip_address, uint8_t dest, uint8_t src, uint8_t ttl,
+void handle_ping_message(int my_mip_address, uint8_t dest, uint8_t src, uint8_t ttl,
                          const uint8_t *sdu, ssize_t sdu_len,
                          struct ethhdr *eh, int if_index);
 
@@ -15,7 +15,7 @@ void handle_arp_message(int raw_sock, int my_mip_address,
                         const uint8_t *sdu, ssize_t sdu_len,
                         const struct ethhdr *eh, int if_index, uint8_t src);
 
-void handle_pong_message(int raw_sock, int my_mip_address,
+void handle_pong_message(int my_mip_address,
                          uint8_t dest, uint8_t src, uint8_t ttl,
                          const uint8_t *sdu, ssize_t sdu_len);
 
