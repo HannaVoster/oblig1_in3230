a4554b42a7c865b399a2b68b0a413ac22f99f9d0
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-10-27 13:24:40 +0100

    tt

--- include/mipd.h
+++ include/mipd.h
@@ -6,26 +6,10 @@
 #include <net/if.h>  //for IFNAMIZ til interface
 
 
-#include "queue.h"
-
 //globale verdier
+#define MAX_EVENTS 10 // epoll
 
 extern int my_mip_address; // MIP-adressen som er tildelt denne daemon-instansen
 extern int debug_mode; // Flag som skrur på/av debug-utskrifter, gis som arg i main
-extern int last_unix_client_fd; // filbeskrivelse til den sist koblede unix klienten
-
-extern int last_ping_src;
-
-extern char last_ping_payload[];
-extern size_t last_ping_payload_len;
-extern int ping_waiting;
-
-
-#define MAX_EVENTS 10 // epoll
-
-extern int last_unix_client_fd; // siste unix klient
-extern int last_ping_src;
-extern int my_mip_address; // min mip addresse
-
 
 #endif
