e3abca2a4a41217cefaa0682a3ecde20202af84f
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-11-01 16:40:32 +0100

    h

--- transport/miptpd.h
+++ transport/miptpd.h
@@ -83,6 +83,7 @@ void unpack_seq_pad(uint16_t seq_pad, uint16_t *seq, uint8_t *padlen);
 uint8_t get_port_from_fd(int fd);
 int remove_app_connection(int fd);
 int register_app_connection(int fd, uint8_t port);
+int get_fd_from_port(uint8_t port);
 
 
 
