0c68b3d8956e67638662bff83764c5869e1923c6
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-11-01 17:42:52 +0100

    h

--- transport/miptpd_utils.c
+++ transport/miptpd_utils.c
@@ -36,7 +36,7 @@ int new_app_connection(int fd, uint8_t port) {
             app_connections[i].app_fd = fd;
             app_connections[i].port = port;
             app_connections[i].next_seq = 0;
-            app_connection[i].last_acked_seq = 0;
+            app_connections[i].last_acked_seq = 0;
             printf("[MIPTPD] Registered app fd=%d on port %d\n", fd, port);
             return 0;
         }
