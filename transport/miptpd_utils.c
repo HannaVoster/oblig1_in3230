3328cdc976cab48476c3821749e07157f7cefa92
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-11-02 10:48:58 +0100

    h

--- transport/miptpd_utils.c
+++ transport/miptpd_utils.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 #include "miptpd.h"
 
 app_connection app_connections[MAX_APPS] = {0}; //liste over app connections
