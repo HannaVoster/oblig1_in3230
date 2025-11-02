fbc07a8e1696d3f75ef436d04b3231984523f2ea
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-11-02 10:50:59 +0100

    h

--- transport/miptpd_utils.c
+++ transport/miptpd_utils.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include "miptpd.h"
 
@@ -118,12 +119,13 @@ uint16_t get_next_seq(int fd) {
 
 void update_last_packet_from_fd(int fd, uint8_t *packet, ssize_t len) {
     for (int i = 0; i < MAX_APPS; i++) {
-        if (app_connections[i].app_fd == fd)
+        if (app_connections[i].app_fd == fd){
             memcpy(app_connections[i].last_packet, packet, len);
             app_connections[i].last_len = len;
             app_connections[i].last_sent_time = time(NULL);
             app_connections[i].waiting_for_ack = 1;
             return;
+        }   
     }
     fprintf(stderr, "[MIPTPD] No connection found for fd=%d (update_last_packet_from_fd)\n", fd);
     return;
