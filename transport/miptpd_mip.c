c5dc97cafa4157807855e403fdc379632c90da29
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-11-02 17:34:17 +0100

    h

--- transport/miptpd_mip.c
+++ transport/miptpd_mip.c
@@ -175,11 +175,11 @@ void handle_incoming_miptp_packet(uint8_t *buf, size_t len, uint8_t src_mip) {
 
     //kopierer ut miptpd header fra buffer
     miptp_hdr_t hdr;
-    memcpy(&hdr, buf, sizeof(hdr));
+    memcpy(&hdr, buf+1, sizeof(hdr));
 
     //lager en peker til payload og beregner lengde
-    uint8_t *payload = buf + sizeof(hdr);
-    size_t payload_len = len - sizeof(hdr);
+    uint8_t *payload = buf +1 + sizeof(hdr);
+    size_t payload_len = len - 1 - sizeof(hdr);
 
     printf("[MIPTPD] Got packet from MIP %d, src_port=%d dst_port=%d len=%zu\n",
            src_mip, hdr.src_port, hdr.dst_port, payload_len);
