d426e07cd09d99babbe75e376feaa90a2ca0a270
Hanna Oster <oster.2118620@studenti.uniroma1.it>
2025-11-10 10:48:50 +0100

    shh

--- transport/src/miptpd_sending.c
+++ transport/src/miptpd_sending.c
@@ -75,8 +75,8 @@ void send_miptp_data(int app_fd, uint8_t *data, size_t len) {
     // ---- Pakker ut felter fra app-meldingen ---
     uint8_t dst_mip  = data[0];  // destinasjons-MIP
     uint8_t dst_port = data[1];  // mottakerens port
-    uint8_t *payload = data + 2; // selve nyttelasten
-    size_t payload_len = len - 2;
+    uint8_t *payload = data; // selve nyttelasten
+    size_t payload_len = len;
 
     // ---- Finner avsenderport og connection ---
     uint8_t src_port = get_port_from_fd(app_fd);
@@ -84,7 +84,7 @@ void send_miptp_data(int app_fd, uint8_t *data, size_t len) {
     if (idx < 0) return; // ugyldig tilkobling
 
     app_connection *connection = &app_connections[idx];
-    connection->peer_mip = dst_mip; // lagrer mottakerens MIP-adresse, brujes av miptpd_retransmit
+    connection->peer_mip = dst_mip; // lagrer mottakerens MIP-adresse, brukes av miptpd_retransmit
 
     // --- Sjekker at vinduet ikke er fullt -----
     if ((connection->next_seq - connection->base_seq) >= MIPTP_WINDOW_SIZE) {
